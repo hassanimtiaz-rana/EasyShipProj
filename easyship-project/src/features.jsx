d26dfb350f82dd4b5740bf25871a4ebc52d11b8a
hassanimtiaz-rana <hassanrana72b@gmail.com>
2023-12-07 00:31:41 +0500

    Added features component
    Added features.jsx

--- easyship-project/src/features.jsx
+++ easyship-project/src/features.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWarehouse, faClock, faBox, faChartBar } from '@fortawesome/free-solid-svg-icons';
+// features component
 
 function renderFeature(title, content, icon) {
   return (
