104a52d36245a8a0f45b4157e9cf37599b2eea8a
AhmadMoiz91 <f200163@cfd.nu.edu.pk>
2023-12-12 21:21:18 +0500

    Migration Update

---
+++ easyShipBackend/easyShipBackend/Models/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace easyShipBackend.Models
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public string Email { get; set; } // Add Email field
+        public string Storename { get; set; } // Add Storename field
+    }
+}
